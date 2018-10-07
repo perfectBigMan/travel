27688798dc64b1547c4a1cf35e57704939d7e67b
superPerfect <316539891@qq.com>
2018-10-07 18:34:31 +0800

    add icon

--- travel/src/pages/home/components/Header.vue
+++ travel/src/pages/home/components/Header.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="header">
-        <div class="header-left">back</div>
-        <div class="header-input">content</div>
-        <div class="header-right">city</div>
+        <div class="header-left"><span class="iconfont back-icon">&#xe624;</span></div>
+        <div class="header-input"><span class="iconfont">&#xe632;</span></div>
+        <div class="header-right">city<span class="iconfont arrow-icon">&#xe62d;</span></div>
     </div>
 </template>
 <script>
@@ -15,19 +15,26 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
   .header
     display: flex
     line-height: .86rem
-    background: #00bcd4
+    background: $bgColor
     color: #fff
     .header-left
       width: .64rem
       float: left
+      .back-icon
+        text-align: center
+        font-size: .4rem
     .header-right
       width: 1.24rem
       float: right
       text-align: center
       border-radius: .1rem
+      .arrow-icon
+        font-size: .24rem
+        padding-left: .1rem
     .header-input
       flex: 1
       background: #fff
@@ -35,5 +42,6 @@ export default {
       height: .64rem
       margin-top: .12rem
       margin-bottom: .12rem
+      padding-left: .2rem
       color: #ccc
 </style>
