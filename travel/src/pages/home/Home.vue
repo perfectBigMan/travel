a8560bd02027f3ae8c140488c3e2331682846a98
superPerfect <316539891@qq.com>
2018-10-07 11:27:04 +0800

    code init

---
+++ travel/src/pages/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="home">
+        <div>home</div>
+        <router-link to="/list">list</router-link>
+    </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style>
+</style>
